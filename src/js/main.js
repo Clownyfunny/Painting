ce8978bdeb22f48cbce187c75e35dae90ca7ade3
pierre <pierre@pierre.pierre>
2014-11-07 13:40:18 +0100

    base of project

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    
+  var scene = new Kinetic.Stage({
+    container: "kinetic",
+    width: 600,
+    height: 400
+  });
+
+  var calque = new Kinetic.Layer();
+
+    // Ici on dessine sur le calque !
+
+  scene.add(calque);
+
+};

5428b0a58cbe405d325647d0b8bbc5127d51cc07
pierre <pierre@pierre.pierre>
2014-11-07 15:56:06 +0100

    version operationnal for colorize

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,50 @@
 window.onload = function() {
-  var colorSelect = '';
-  var scene = new Kinetic.Stage({
-    container: "kinetic",
-    width: 1024,
-    height: 800
-  });
-  var drawing = new Kinetic.Layer({
-    y: 20,
-    scale: 0.6
-  });
-  for(var key in datas) {
+    var colorSelect = '';
+    var scene = new Kinetic.Stage({
+        container: "kinetic",
+        width: 1024,
+        height: 550
+    });
+    var drawing = new Kinetic.Layer({
+        y: 20,
+        scale: 0.6
+    });
+    var colorSelection = new Kinetic.Layer({});
+    for(var key in datas) {
         var path = new Kinetic.Path({
-          data: datas[key].path,
-          fill: '#eee',
-          stroke: '#555',
-          strokeWidth: 1
+            data: datas[key].path,
+            fill: '#FFF', 
+            stroke: '#000',
+            strokeWidth: 3
         });
 
         path.on("click", function(){
-        	if(colorSelect==''){
-        		this.setFill(colorSelect);
-        		drawing.drawScene();
-        	}
+            if(colorSelect!=''){
+                this.setFill(colorSelect);
+                drawing.drawScene();
+            }
         });
         drawing.add(path);
     }
+    console.log(maPalette.length);
+    for(var colorize in maPalette){
+        console.log(maPalette[colorize]);
+        var colorRect = new Kinetic.Rect({
+            fill: maPalette[colorize],
+            width: 60,
+            height : 60,
+            stroke: '#000',
+            strokeWidth: 2,
+            x: 700+Math.floor(colorize/5)*60,
+            y: 50+(colorize%5)*60
+        });
+        colorRect.on("click", function(){
+            console.log(this);
+            colorSelect=this.fill();
+        });
+        colorSelection.add(colorRect);
+    }
 
-  scene.add(drawing);
-
+    scene.add(drawing);
+    scene.add(colorSelection);
 };
